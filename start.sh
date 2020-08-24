2d802c39c3686ebba1893b277a217c998e800357
Maksym Movchan <xondev@vivaldi.net>
2020-08-24 17:55:23 +0300

    test: Remove dialog

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
-# Luke's Auto Rice Boostrapping Script (LARBS)
-# by Luke Smith <luke@lukesmith.xyz>
-# License: GNU GPLv3
+#!/bin/bash
 
-# Opions handling
-while getopts ":a:r:b:p:h" o; do case "${o}" in
+while getopts ":h:d:b:c:a" o; do case "${o}" in
 	h) printf "[-d dotfiles ] [-c csv ] [-a AUR helper ] [ -h this message ] \\n" && exit ;;
 	d) dotfiles=${OPTARG} && git ls-remote "$dotfiles" || exit ;;
 	b) branch=${OPTARG} ;;
@@ -27,37 +23,57 @@ installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 error() { clear; printf "ERROR:\\n%s\\n" "$1"; exit;}
 
 welcomemsg() { \
-	dialog --title "Welcome!" --msgbox "Welcome to Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n" 10 60
-	dialog --colors --title "Important Note!" --yes-label "All ready!" --no-label "Return..." --yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
+	read -p "Welcome to installation script" -r
+	read -p "Continue? y/N" -n 1 -r
+	echo
+	if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+		error "Bye"
+	fi
 }
 
 getuserandpass() { \
-	# Prompts user for new username an password.
-	name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
+	name=''
+	read -p 'Account name: ' name -r
 	while ! echo "$name" | grep "^[a-z_][a-z0-9_-]*$" >/dev/null 2>&1; do
-		name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+		read -p 'Not valid username: ' name -r
 	done
-	pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
-	pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	pass1=''; pass2=''
+	read -p 'Password:' -sr
+	read -p 'Retype:' -sr
 	while ! [ "$pass1" = "$pass2" ]; do
 		unset pass2
-		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done ;
+		echo 'Passwords not match'
+		read -p 'Password:' -sr
+		read -p 'Retype:' -sr
+	done
 }
 
 usercheck() { \
 	! (id -u "$name" >/dev/null) 2>&1 ||
-	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. LARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nLARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that LARBS will change $name's password to the one you just gave." 14 70
+	{ read -p 'Warning! User exists in system. Continue?' -r
+	read -p "Continue? y/N" -n 1 -r
+	echo
+	if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+		error "Bye"
+		exit 1
+	fi; }
 }
 
 preinstallmsg() { \
-	dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit; }
+	read -p "Start installation? y/N" -n 1 -r
+	echo
+	if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+		error "Bye"
+		exit 1
+	fi
 }
 
 adduserandpass() { \
 	# Adds user `$name` with password $pass1.
-	dialog --infobox "Adding user \"$name\"..." 4 50
+	echo "Adding user \"$name\"..."
 	useradd -m -g wheel -s /bin/bash "$name" >/dev/null 2>&1 ||
 	usermod -a -G wheel "$name" && mkdir -p /home/"$name" \
 						/home/"$name"/vids \
@@ -70,22 +86,22 @@ adduserandpass() { \
 						/home/"$name"/work && chown -R "$name":wheel /home/"$name"
 	repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel $(dirname "$repodir")
 	echo "$name:$pass1" | chpasswd
-	unset pass1 pass2 ;
+	unset pass1 pass2
 }
 
 refreshkeys() { \
-	dialog --infobox "Refreshing Arch Keyring..." 4 40
+	echo "Refreshing Keyring"
 	pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
 }
 
 newperms() { # Set special sudoers settings for install (or after).
-	sed -i "/#LARBS/d" /etc/sudoers
-	echo "$* #LARBS" >> /etc/sudoers ;
+	sed -i "/#XON/d" /etc/sudoers
+	echo "$* #XON" >> /etc/sudoers ;
 }
 
 manualinstall() { # Installs $1 manually if not installed. Used only for AUR helper here.
 	[ -f "/usr/bin/$1" ] || (
-	dialog --infobox "Installing \"$1\", an AUR helper..." 4 50
+	echo "Installing \"$1\", an AUR helper..."
 	cd /tmp || exit
 	rm -rf /tmp/"$1"*
 	curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/"$1".tar.gz &&
@@ -96,14 +112,14 @@ manualinstall() { # Installs $1 manually if not installed. Used only for AUR hel
 }
 
 maininstall() {
-	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
+	echo "Installing \`$1\` ($n of $total). $1 $2"
 	installpkg "$1"
 }
 
 gitmakeinstall() {
 	progname="$(basename "$1" .git)"
 	dir="$repodir/$progname"
-	dialog --title "LARBS Installation" --infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
+	echo "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2"
 	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return ; sudo -u "$name" git pull --force origin master;}
 	cd "$dir" || exit
 	make >/dev/null 2>&1
@@ -112,13 +128,13 @@ gitmakeinstall() {
 }
 
 aurinstall() { \
-	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
+	echo "Installing \`$1\` ($n of $total) from the AUR. $1 $2"
 	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
 	sudo -u "$name" $helper -S --noconfirm "$1" >/dev/null 2>&1
 }
 
 pipinstall() { \
-	dialog --title "LARBS Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
+	echo "Installing the Python package \`$1\` ($n of $total). $1 $2"
 	command -v pip || installpkg python-pip >/dev/null 2>&1
 	yes | pip install "$1"
 }
@@ -140,7 +156,7 @@ installationloop() { \
 }
 
 putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
-	dialog --infobox "Downloading and installing config files..." 4 60
+	echo "Downloading and installing config files..."
 	[ -z "$3" ] && branch="master" || branch="$repobranch"
 	dir=$(mktemp -d)
 	[ ! -d "$2" ] && mkdir -p "$2"
@@ -160,13 +176,13 @@ additional() {
 	sed -i "s/\/home\/xon/\/home\/$name/" /usr/share/libalpm/hooks/90-pacwall.hook
 }
 
-systembeepoff() { dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
+systembeepoff() {
+	echo "Getting rid of that retarded error beep sound..."
 	rmmod pcspkr
 	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;
 }
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-installpkg dialog || error "Are you sure you're running this as the root user and have an internet connection?"
 welcomemsg        || error "User exited."
 getuserandpass    || error "User exited."
 usercheck         || error "User exited."
@@ -174,19 +190,16 @@ preinstallmsg     || error "User exited."
 adduserandpass    || error "Error adding username and/or password."
 refreshkeys       || error "Error automatically refreshing Arch keyring. Consider doing so manually."
 
-dialog --title "LARBS Installation" --infobox "Preparing install." 5 70
+echo "Preparing install."
 installpkg curl
 installpkg base-devel
 installpkg git
 installpkg ntp
 
-dialog --title "LARBS Installation" --infobox "Synchronizing system time to ensure successful and secure installation of software..." 4 70
+echo "Synchronizing system time to ensure successful and secure installation of software..."
 ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
 
-[ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
-
-# Allow user to run sudo without password. Since AUR programs must be installed
-# in a fakeroot environment, this is required for all builds with AUR.
+[ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers
 newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
 
 # Make pacman and yay colorful and adds eye candy on the progress bar because why not.
@@ -200,17 +213,17 @@ manualinstall $helper || error "Failed to install AUR helper."
 
 installationloop
 
-dialog --title "LARBS Installation" --infobox "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes." 5 70
+echo "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes."
 #yes | sudo -u "$name" $helper -S libxft-bgra >/dev/null 2>&1
 
 #?#?#?#?#?#?#?#?#?#?#?#?#?#?#?#?#?#?
-git clone https://gitlab.freedesktop.org/xorg/lib/libxft.git libxft >/dev/null 2>&1
+git clone https://gitlab.freedesktop.org/xorg/lib/libxft.git libxft &>/dev/null
 cd libxft
 wget -qO- 'https://gitlab.freedesktop.org/xorg/lib/libxft/merge_requests/1.patch' | patch -p1
-./autogen.sh
-./configure --prefix=/usr --sysconfdir=/etc --disable-static
+./autogen.sh &>/dev/null
+./configure --prefix=/usr --sysconfdir=/etc --disable-static &>/dev/null
 make >/dev/null 2>&1
-make install >/dev/null 2>&1
+make install &>/dev/null
 cd /tmp
 #?#?#?#?#?#?#?#?#?#?#?#?#?#?#?#?#?#?
 
@@ -237,5 +250,5 @@ killall pulseaudio; sudo -u "$name" pulseaudio --start
 %wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm"
 
 # Last message! Install complete!
-dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Luke" 12 80
+echo "Congrats! Installation successfull"
 clear
